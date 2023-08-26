fcf7e7f8307830f45aef532ddc67fbf68c3fbd2b
E223716U <lucas.jonnard@etu.univ-nantes.fr>
2023-08-26 18:00:46 +0200

    Ajout d'animations d'apparition / disparition des titres de partie

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cadres = [];
+const titres = [];
 
 function set_and_get_frames_positions() {
     const cadre_1 = document.getElementById("cadre_1");
@@ -28,6 +29,24 @@ function set_and_get_frames_positions() {
     const cadre_7 = document.getElementById("cadre_7");
     cadre_7.style.left = "-100%";
     cadres.push(cadre_7);
+
+    const cadre_8 = document.getElementById("cadre_8");
+    cadre_8.style.right = "-200%";
+    cadres.push(cadre_8);
+}
+
+function get_titles() {
+    const titre_a_propos = document.getElementById("titre_a_propos");
+    titre_a_propos.style.opacity = "0";
+    titres.push(titre_a_propos);
+
+    const titre_competences = document.getElementById("titre_competences");
+    titre_competences.style.opacity = "0";
+    titres.push(titre_competences);
+
+    const titre_projets_personnels = document.getElementById("titre_projets_personnels");
+    titre_projets_personnels.style.opacity = "0";
+    titres.push(titre_projets_personnels);
 }
 
 function scroll_function() {
@@ -47,10 +66,21 @@ function scroll_function() {
             cadres[i].style.animation = "cadre_" + direction + "_disparition 2s ease-in-out forwards";
         }
     }
+
+    // Animation des titres
+    for (const titre of titres) {
+        const rect = titre.getBoundingClientRect();
+        if (window.innerHeight > rect.top + rect.height / 1.5) {
+            titre.style.animation = "titre_apparition 2s ease-in-out forwards";
+        } else if (titre.style.animation !== "titre_disparition 2s ease-in-out forwards") {
+            titre.style.animation = "titre_disparition 2s ease-in-out forwards";
+        }
+    }
 }
 
 
 set_and_get_frames_positions();
+get_titles();
 scroll_function();
 document.onscroll = () => {
     scroll_function();
